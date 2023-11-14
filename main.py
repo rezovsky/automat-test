385067603970d1309ebe434f2ddbe54cfada2bcd
rezovskiy_em <e.rezovsky@gmail.com>
2023-11-15 03:51:21 +0500

    end

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from time import sleep
-
-from pywinauto import keyboard
+from pywinauto import keyboard, Desktop
 from pywinauto.application import Application
-
 import configparser
-
-
-chrome_app = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 
 
 def open_app(type, link, links):
@@ -14,15 +8,24 @@ def open_app(type, link, links):
         action_key = "{RIGHT}"
     if type == "v":
         action_key = "{UP}"
-    app = Application().start(chrome)
+    app = Application().start(chrome_app)
     app = Application(backend="uia").connect(title_re='Новая', timeout=5)
     keyboard.send_keys(link.get('links', links) + "{ENTER}")
-    keyboard.send_keys(
-        "{VK_SHIFT down}{LWIN down}" + action_key + "{LWIN up}{VK_SHIFT up}{F11}"
-    )
+    keyboard.send_keys("{VK_SHIFT down}{LWIN down}" + action_key + "{LWIN up}{VK_SHIFT up}{F11}^{F5}")
+
+
+def close_chrome():
+    chromes = Desktop(backend="uia").windows(title_re=".* Google Chrome$")
+    if len(chromes) == 0:
+        return
+    for chrome in chromes:
+        chrome.set_focus()
+        keyboard.send_keys("%{F4}")
 
 
 if __name__ == '__main__':
+    close_chrome()
+
     settings = configparser.ConfigParser()
     settings.read('settings.ini')
     link = configparser.ConfigParser()
@@ -30,7 +33,7 @@ if __name__ == '__main__':
 
     monitors_count = int(settings.get('settings', 'monitors'))
     orientation = settings.get('settings', 'orientation')
-    chrome = settings.get('settings', 'chrome')
+    chrome_app = settings.get('settings', 'chrome')
 
     links = settings.get('links', 'links').split(',')
     for count in range(monitors_count):
