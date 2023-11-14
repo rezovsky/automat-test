247c201848b4559013e21bd618fb8b392fd98d77
rezovskiy_em <e.rezovsky@gmail.com>
2023-11-15 03:24:39 +0500

    end

--- main.py
+++ main.py
@@ -3,26 +3,35 @@ from time import sleep
 from pywinauto import keyboard
 from pywinauto.application import Application
 
+import configparser
+
+
 chrome_app = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 
-monitors_count = 3
 
-links = ('https://vk.com', 'https://sima-land.com', 'https://youtube.com')
-
-
-def open_app(count, type="g"):
+def open_app(type, link, links):
     if type == "g":
         action_key = "{RIGHT}"
     if type == "v":
         action_key = "{UP}"
-    app = Application(backend="uia").start(chrome_app)
-    sleep(.5)
-    keyboard.send_keys(links[count] + "{ENTER}")
+    app = Application().start(chrome)
+    app = Application(backend="uia").connect(title_re='Новая', timeout=5)
+    keyboard.send_keys(link.get('links', links) + "{ENTER}")
     keyboard.send_keys(
         "{VK_SHIFT down}{LWIN down}" + action_key + "{LWIN up}{VK_SHIFT up}{F11}"
     )
 
 
 if __name__ == '__main__':
+    settings = configparser.ConfigParser()
+    settings.read('settings.ini')
+    link = configparser.ConfigParser()
+    link.read('links.ini')
+
+    monitors_count = int(settings.get('settings', 'monitors'))
+    orientation = settings.get('settings', 'orientation')
+    chrome = settings.get('settings', 'chrome')
+
+    links = settings.get('links', 'links').split(',')
     for count in range(monitors_count):
-        open_app(count, "g")
+        open_app(orientation, link, links[count])
